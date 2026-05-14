0b893dbaa096f2da36ae6232b61594d8243ea5a8
kiruthika <kiruthikakokila3@gmail.com>
2026-05-14 11:27:34 +0530

    added index.html

--- server.js
+++ server.js
@@ -1,48 +1,42 @@
 const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
-const mongoose = require("mongoose");
-
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
-
-// MongoDB
-mongoose.connect("mongodb://127.0.0.1:27017/chat");
-
-// Schema
-const Msg = mongoose.model("Msg", {
-  user: String,
-  text: String
+const http = require("http").createServer(app);
+const io = require("socket.io")(http, {
+  cors: {
+    origin: "*"
+  }
 });
 
-// static
 app.use(express.static("public"));
 
-// users
+// default page
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+// socket connection
 let users = [];
 
 io.on("connection", (socket) => {
 
-  socket.on("join", (user) => {
-    users.push(user);
+  socket.on("join", (name) => {
+    users.push(name);
     io.emit("online", users.length);
   });
 
-  socket.on("msg", async (data) => {
-    await Msg.create(data);
-    io.emit("msg", data);
+  socket.on("message", (data) => {
+    io.emit("message", data);
   });
 
-  socket.on("typing", (name)=>{
-    socket.broadcast.emit("typing", name + " typing...");
-  });
-
-  socket.on("disconnect", ()=>{
+  socket.on("disconnect", () => {
     users.pop();
     io.emit("online", users.length);
   });
 
 });
 
-server.listen(3000, ()=>console.log("Server running"));
+const PORT = process.env.PORT || 3000;
+
+http.listen(PORT, () => {
+  console.log("Server running on " + PORT);
+});
