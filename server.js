1e0086bdd569bd5b3b5429251cdb4c626becdd0d
kiruthika <kiruthikakokila3@gmail.com>
2026-05-14 09:26:09 +0530

    first commit

--- server.js
+++ server.js
@@ -1,40 +1,167 @@
-const express = require("express");
-const http = require("http");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const dotenv = require("dotenv");
+// server.js
 
-dotenv.config();
+const express = require("express")
+const http = require("http")
+const { Server } = require("socket.io")
 
-const app = express();
-const server = http.createServer(app);
+const app = express()
+const server = http.createServer(app)
+const io = new Server(server)
 
-// SOCKET
-const io = require("socket.io")(server, {
-  cors: { origin: "*" }
-});
-require("./socket/socket")(io);
+app.use(express.static(__dirname))
 
-// MIDDLEWARE
-app.use(cors());
-app.use(express.json());
-app.use(express.static("public"));
-app.use("/uploads", express.static("uploads"));
+// ROOMS
+let rooms = {}
 
-// DB
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("MongoDB Connected"))
-.catch(err => console.log(err));
+io.on("connection", (socket)=>{
 
-// ROUTES
-app.use("/api/auth", require("./routes/auth"));
-app.use("/api/users", require("./routes/users"));
-app.use("/api/messages", require("./routes/messages"));
-app.use("/api/upload", require("./routes/upload"));
+console.log("User Connected")
 
-// PORT
-const PORT = process.env.PORT || 3000;
+// CREATE ROOM
+socket.on("create room", (data)=>{
 
-server.listen(PORT, () => {
-  console.log("Server running on " + PORT);
-});
+const {
+roomName,
+password,
+type,
+userName
+} = data
+
+// CREATE ROOM
+rooms[roomName] = {
+password,
+type,
+users:[userName]
+}
+
+socket.join(roomName)
+
+socket.room = roomName
+socket.userName = userName
+
+socket.emit("room joined", roomName)
+
+io.to(roomName).emit(
+"room users",
+rooms[roomName].users
+)
+
+})
+
+// JOIN ROOM
+socket.on("join room", (data)=>{
+
+const {
+roomName,
+password,
+userName
+} = data
+
+const room = rooms[roomName]
+
+if(!room){
+socket.emit("error message","Room Not Found")
+return
+}
+
+// PASSWORD CHECK
+if(room.password !== password){
+socket.emit("error message","Wrong Password")
+return
+}
+
+// PRIVATE ROOM ONLY 2 USERS
+if(
+room.type === "private" &&
+room.users.length >= 2
+){
+socket.emit("error message",
+"Private Room Full"
+)
+return
+}
+
+// JOIN
+room.users.push(userName)
+
+socket.join(roomName)
+
+socket.room = roomName
+socket.userName = userName
+
+socket.emit("room joined", roomName)
+
+io.to(roomName).emit(
+"room users",
+room.users
+)
+
+})
+
+// CHAT MESSAGE
+socket.on("chat message",(msg)=>{
+
+if(socket.room){
+
+socket.to(socket.room).emit(
+"chat message",
+{
+name:socket.userName,
+message:msg
+}
+)
+
+}
+
+})
+
+// TYPING
+socket.on("typing", ()=>{
+
+socket.to(socket.room).emit(
+"typing",
+socket.userName
+)
+
+})
+
+// STOP TYPING
+socket.on("stop typing", ()=>{
+
+socket.to(socket.room).emit(
+"stop typing"
+)
+
+})
+
+// DISCONNECT
+socket.on("disconnect", ()=>{
+
+const roomName = socket.room
+
+if(roomName && rooms[roomName]){
+
+rooms[roomName].users =
+rooms[roomName].users.filter(
+u => u !== socket.userName
+)
+
+io.to(roomName).emit(
+"room users",
+rooms[roomName].users
+)
+
+// DELETE EMPTY ROOM
+if(rooms[roomName].users.length === 0){
+delete rooms[roomName]
+}
+
+}
+
+})
+
+})
+
+server.listen(3000, ()=>{
+console.log("Running on 3000")
+})
