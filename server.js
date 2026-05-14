5e1ca31201cc058c6860a3837eedbc33a6ee18db
kiruthika <kiruthikakokila3@gmail.com>
2026-05-14 09:38:52 +0530

    full chat app login + public/private

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 const path = require("path");
 
-// static folder
 app.use(express.static(path.join(__dirname, "public")));
 
-// default route
+require("./socket/socket")(io);
+
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("Server running on " + PORT));
+http.listen(PORT, () => console.log("Server running on " + PORT));
